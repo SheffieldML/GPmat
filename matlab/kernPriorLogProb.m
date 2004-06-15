03d39ea75e148b2a3866591692ba4dc95d6055de
Neil Lawrence <lawrennd@gmail.com>
2004-06-15 15:01:55 +0000

    Added file.

---
+++ matlab/kernPriorLogProb.m
@@ -0,0 +1,24 @@
+function L = kernPriorLogProb(kern)
+
+% KERNPRIORLOGPROB Compute penalty terms associated with kernel priors.
+
+% KERN
+
+% KERN
+
+
+L = 0;
+switch kern.type
+ case 'cmpnd'
+  for i = 1:length(kern.comp)
+    L = L + kernPriorLogProb(kern.comp{i});
+  end
+ otherwise
+  if isfield(kern, 'priors')
+    params = feval([kern.type 'KernExtractParam'], kern);
+    for i = 1:length(kern.priors)
+      index = kern.priors(i).index;
+      L = L + priorLogProb(kern.priors(i), params(index));
+    end
+  end
+end
