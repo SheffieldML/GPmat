7b95591e4a632135a812a75658feff25f36ec51a
Neil Lawrence <lawrennd@gmail.com>
2004-07-12 20:28:09 +0000

    Large scale commit.

--- matlab/priorGradient.m
+++ matlab/priorGradient.m
@@ -1,6 +1,6 @@
 function g = priorGradient(prior, params)
 
-% PRIORGRADIENTPARAM Gradient of the prior with respect to its variables
+% PRIORGRADIENT Gradient of the prior with respect to its variables
 
 % PRIOR
 
