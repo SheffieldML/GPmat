f5f76d50b84c1394fd54c86f2e473bf12a965bec
Neil Lawrence <lawrennd@gmail.com>
2005-07-27 15:15:32 +0000

    Some missing noise files.

---
+++ matlab/noiseReadParamsFromFID.m
@@ -0,0 +1,39 @@
+function noise = noiseReadParamsFromFID(type, FID)
+
+% NOISEREADPARAMSFROMFID Read the noise parameters from C++ file FID.
+
+% NOISE
+
+
+noise = noiseCreate(type);
+lineStr = getline(FID);
+tokens = tokenise(lineStr, '=');
+if(length(tokens)~=2 | ~strcmp(tokens{1}, 'numParams'))
+  error('Incorrect file format.')
+end
+noise.nParams = str2num(tokens{2});
+  
+lineStr = getline(FID);
+tokens = tokenise(lineStr, '=');
+if(length(tokens)~=2 | ~strcmp(tokens{1}, 'numProcesses'))
+  error('Incorrect file format.')
+end
+noise.numProcess = str2num(tokens{2});
+
+if strcmp(type, 'ncnm') 
+  lineStr = getline(FID);
+  tokens = tokenise(lineStr, '=');
+  if(length(tokens)~=2 | ~strcmp(tokens{1}, 'gammaSplit'))
+    error('Incorrect file format.')
+  end
+  noise.gammaSplit = str2num(tokens{2});
+end
+noise = noiseParamInit(noise);
+  
+lineStr = getline(FID);
+tokens = tokenise(lineStr);
+params = zeros(1, noise.nParams);
+for j=1:noise.nParams
+  params(1, j) = str2num(tokens{j});
+end
+noise = feval([noise.type 'NoiseExpandParam'], noise, params);
