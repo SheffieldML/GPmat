bf82a2851f90b459d046841a5a9dabb9358fb8a2
Neil Lawrence <lawrennd@gmail.com>
2006-10-14 21:25:19 +0000

    Commit for release vrs 0.131.

--- matlab/normuniPriorLogProb.m
+++ matlab/normuniPriorLogProb.m
@@ -7,6 +7,6 @@ function l = normuniPriorLogProb(prior, x)
 % Compute log prior
 u = (x+prior.width/2)/prior.sigma;
 uprime = u - prior.width/prior.sigma;
-l = sum(- log(prior.width) - lnDiffCumGaussian(u, uprime));
+l = sum(- log(prior.width) + lnDiffCumGaussian(u, uprime));
 
 
