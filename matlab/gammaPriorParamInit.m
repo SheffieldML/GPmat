6300d56a4a8477af2b5eb4c3ec6e87391a5c6fe7
Neil Lawrence <lawrennd@gmail.com>
2007-08-28 06:52:39 +0000

    Files before July/August udpates.

--- matlab/gammaPriorParamInit.m
+++ matlab/gammaPriorParamInit.m
@@ -1,6 +1,15 @@
 function prior = gammaPriorParamInit(prior)
 
 % GAMMAPRIORPARAMINIT Gamma prior model's parameter initialisation.
+% FORMAT
+% DESC initialises the parameters of the gamma prior with some
+% default parameters.
+% ARG prior : prior structure to be initialised.
+% RETURN prior : prior structure with initial values in place.
+% 
+% SEEALSO : priorCreate
+%
+% COPYRIGHT : Neil D. Lawrence, 2004, 2005, 2006
 
 % PRIOR
 
@@ -8,5 +17,5 @@ prior.a = 1e-6;
 prior.b = 1e-6;
 
 prior.transforms.index = [1 2];
-prior.transforms.type = 'negLogLogit';
+prior.transforms.type = optimiDefaultConstraint('positive');
 prior.nParams = 2;
--- matlab/invgammaPriorParamInit.m
+++ matlab/invgammaPriorParamInit.m
@@ -1,8 +1,15 @@
 function prior = invgammaPriorParamInit(prior)
 
 % INVGAMMAPRIORPARAMINIT Inverse gamma prior model's parameter initialisation.
-
-% PRIOR
+% FORMAT
+% DESC initialises the parameters of the inverse gamma prior with some
+% default parameters.
+% ARG prior : prior structure to be initialised.
+% RETURN prior : prior structure with initial values in place.
+% 
+% SEEALSO : priorCreate, gammaPriorParamInit
+%
+% COPYRIGHT : Neil D. Lawrence, 2004, 2005, 2006
 
 % PRIOR
 
@@ -11,5 +18,5 @@ prior.a = 1e-6;
 prior.b = 1e-6;
 
 prior.transforms.index = [1 2];
-prior.transforms.type = 'negLogLogit';
+prior.transforms.type =  optimiDefaultConstraint('positive');
 prior.nParams = 2;
