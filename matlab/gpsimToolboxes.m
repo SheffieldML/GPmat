2c2e776fbfef305a7c3c3d29c0b70e1309bcca4a
lawrennd <lawrennd@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-06-04 16:31:09 +0000

    Added gpsim matlab files.

---
+++ matlab/gpsimToolboxes.m
@@ -0,0 +1,8 @@
+% GPSIMTOOLBOXES Toolboxes for the GPSIM software.
+
+importLatest('netlab');
+importLatest('optimi');
+importLatest('ndlutil');
+importLatest('mltools');
+importLatest('kern');
+importLatest('minimize');
