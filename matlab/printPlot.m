1ea330a3acbb2665771349f771ee591d7d5c5d43
lawrennd <lawrennd@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2010-04-10 09:05:46 +0000

    Changes to MLSP paper.

--- matlab/printPlot.m
+++ matlab/printPlot.m
@@ -12,41 +12,56 @@ function printPlot(fileName, directory, directoryHtml)
 % COPYRIGHT : Neil D. Lawrence, 2008
   
 % NDLUTIL
-  
-if nargin < 2 
-  directory = '.';
-end
-if nargin < 3
-  png = false;
-else
-  png = true;
-end
-fprintf('Printing eps plot ...\n');
-print('-depsc', [directory filesep fileName '.eps']) % OCTAVE doesn't
-                                                     % include .eps automatically
-cmap = colormap;
-if png
-  fprintf('Printing png plot ...\n');
-  % make smaller for PNG plot.
-  pos = get(gcf, 'paperposition');
-  origpos = pos;
-  pos(3) = pos(3)/2;
-  pos(4) = pos(4)/2;
-  set(gcf, 'paperposition', pos);
-  fontsize = get(gca, 'fontsize');
-  set(gca, 'fontsize', fontsize/2);
-  lineWidth = get(gca, 'lineWidth');
-  set(gca, 'lineWidth', lineWidth*2);
-  print('-dpng', [directoryHtml filesep fileName '.png']);
-  set(gcf, 'paperposition', origpos);
-  set(gca, 'fontsize', fontsize);
-  set(gca, 'lineWidth', lineWidth);
-end
 
-fprintf('Printing black and white eps plot ...\n');
-colormap gray
-print('-deps', [directory filesep fileName 'NoColour.eps']) % Octave
-                                                            % doesn't
-                                                            % include
-                                                            % .eps automatically
-colormap(cmap);
+  global printDiagram
+
+  if isempty(printDiagram)
+    printTrue = true; % User hasn't been explicit, assume print.
+  else
+    printTrue = printDiagram; % Use user preference.
+  end
+  if printTrue
+    if nargin < 2 
+      directory = '.';
+    end
+    if nargin < 3
+      png = false;
+    else
+      png = true;
+    end
+    fprintf('Printing eps plot ...\n');
+    print('-depsc', [directory filesep fileName '.eps']) % OCTAVE doesn't
+                                                       % include .eps automatically
+    if isoctave
+      % make a plot with text separated.
+      fprintf('Printing epslatex plot ...\n');
+      print('-depslatex', [directory filesep fileName '_latex.tex']) 
+    end
+    cmap = colormap;
+    if png
+      fprintf('Printing png plot ...\n');
+      % make smaller for PNG plot.
+      pos = get(gcf, 'paperposition');
+      origpos = pos;
+      pos(3) = pos(3)/2;
+      pos(4) = pos(4)/2;
+      set(gcf, 'paperposition', pos);
+      fontsize = get(gca, 'fontsize');
+      set(gca, 'fontsize', fontsize/2);
+      lineWidth = get(gca, 'lineWidth');
+      set(gca, 'lineWidth', lineWidth*2);
+      print('-dpng', [directoryHtml filesep fileName '.png']);
+      set(gcf, 'paperposition', origpos);
+      set(gca, 'fontsize', fontsize);
+      set(gca, 'lineWidth', lineWidth);
+    end
+    
+    fprintf('Printing black and white eps plot ...\n');
+    colormap gray
+    print('-deps', [directory filesep fileName 'NoColour.eps']) % Octave
+                                                                % doesn't
+                                                                % include
+                                                                % .eps automatically
+    colormap(cmap);
+  end
+end
