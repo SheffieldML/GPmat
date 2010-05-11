cc1a3afa06e8da6a0f02dd0851d3a239660f1925
alkalait <alkalait@gmail.com>
2010-05-11 21:23:23 +0000

    R versions of a few gp* functions

---
+++ R/gpComputeAlpha.R
@@ -0,0 +1,58 @@
+gpComputeAlpha <- function(model, m) {
+
+  if (nargs < 2)
+    m = model$m
+
+  if (model$approx == "ftc") {
+    model$alpha = matrix(0,model$N,model$d) #zeros(model.N, model.d)
+    if (!"isSpherical" %in% names(model) || model$isSpherical)
+      model$alpha = model$invK_uu %*% m
+    else {
+      for (i in c(1:model$d)) {
+	ind = gpDataIndices(model, i)
+	model$alpha[ind, i] = model$invK_uu[[i]] %*% m[ind, i]
+      }
+    }
+  }
+  else if (model$approx %in% c("dtc","dtcvar")) {
+    model$alpha = matrix(0, model$k, model$d)
+    if (!("isSpherical" %in% names(model)) || model$isSpherical)
+      model$alpha = model$Ainv %*% model$K_uf %*% m
+    else {
+      for (i in c(1:model$d)) {
+	ind = gpDataIndices(model, i)
+	model$alpha[,i] = model$Ainv[[i]] %*% model$K_uf[,ind] %*% m[ind,i]
+      }
+    }
+  }
+  else if (model$approx == "fitc") {
+    model$alpha = matrix(0, model$k, model$d)
+    if (!("isSpherical" %in% names(model)) || model$isSpherical)
+      model$alpha = model$Ainv %*% model$K_uf %*% model$Dinv %*% m
+    else {
+      for (i in c(1:model$d)) {
+	ind = gpDataIndices(model, i)
+	model$alpha[,i] = model$Ainv[[i]]
+	    %*% model$K_uf[,ind] %*% model$Dinv[[i]] %*% m[ind,i]
+      }
+    }
+  else if (model$approx == "pitc") {
+    model$alpha = matrix(0, model$k, model$d)
+    if (!("isSpherical" %in% names(model)) || model$isSpherical)
+      for (i in seq(along=model$blockEnd)) {
+	ind = gpBlockIndices(model, i)
+	model$alpha = model$alpha + model$Ainv%*%model$K_uf[,ind]%*%model$Dinv[[i]]%*%m[ind,]
+      }
+    else {
+      for (i in seq(along=model.blockEnd)) {
+	for (j in c(1:model$d)) {
+	  ind = gpDataIndices(model, j, i)
+	  model$alpha[,j] = model$alpha[,j] + model$Ainv[[j]]%*%model$K_uf[,ind]%*%
+	      model$Dinv[[i,j]]%*%m[ind,j]
+	}
+      }
+    }
+  }
+
+  return(model)
+}
---
+++ R/gpDataIndices.R
@@ -0,0 +1,31 @@
+gpDataIndices <- function(model, dimNo, blockNo) {
+
+  if (nargs > 2) {
+    if (model$approx != "pitc")
+      stop("Block number only relevant for pitc approximation.")
+    else {
+      if (blockNo == 1)
+	startVal = 1
+      else
+	startVal = model$blockEnd[blockNo-1]+1
+      endVal = model$blockEnd[blockNo]
+      if (model$isMissingData) {
+	st = min(which(model$indexPresent[[dimNo]] >= startVal))
+	fi = max(which(model$indexPresent[[dimNo]] <= endVal))
+	ind = t(model$indexPresent[[dimNo]][st:fi])
+      } else
+	ind = startVal:endVal
+    }
+  } else {
+    if (model$approx == "pitc")
+      stop("Must give block number with PITC approximation.")
+    else {
+      if (model$isMissingData)
+	ind = t(model$indexPresent[[dimNo]])
+      else
+	ind = 1:model$N
+    }
+  }
+
+  return(ind)
+}
