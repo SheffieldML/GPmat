baf1b5ee11da10217b252410aa907f68960e45e7
alkalait <alkalait@gmail.com>
2010-06-02 10:13:27 +0000

    Minor bug-fixes in ~/mlprojects/gpsim/R/R/{kernFunctions.R, whiteKernFunctions.R}.

--- R/R/kernFunctions.R
+++ R/R/kernFunctions.R
@@ -237,10 +237,10 @@ kernDiagGradX <- function (kern, x) {
 
 
 
-kernGradX <- function (kern, x) {
+kernGradX <- function (kern, x, x2) {
   funcName <- paste(kern$type, "KernGradX", sep="")
   func <- get(funcName, mode="function")
-  k <- func(kern, x)
+  k <- func(kern, x, x2)
   return (k)
 }
 
