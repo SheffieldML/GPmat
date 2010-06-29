34730093d647e2a0aa8c206947cc94c724d26399
maalvarezl <malvarez@utp.edu.co>
2010-06-29 17:31:54 +0000

    Update of multiKernBlockGradientX.m

--- matlab/multiKernGradientBlockX.m
+++ matlab/multiKernGradientBlockX.m
@@ -1,4 +1,4 @@
-function gX = multiKernGradientBlockX(kern, X, X2, i, j)
+function gX = multiKernGradientBlockX(kern, X, X2, covGrad, i, j)
 
 % MULTIKERNGRADIENTBLOCKX
 % FORMAT
@@ -33,21 +33,21 @@ function gX = multiKernGradientBlockX(kern, X, X2, i, j)
 % SEEALSO : multiKernCreate, multiKernGradX
 %
 % COPYRIGHT : Neil D. Lawrence, 2006
+%
+% MODIFICATIONS : Mauricio A. Alvarez, 2010
 
 % KERN
 
-if nargin < 5
-    j = i;
-    i = X2;
-    X2 = [];
+if nargin < 6
+  j = i;
+  i = covGrad;
+  covGrad = X2;
+  X2 = [];
 end
-outArg = 2;
+
 if i == j
     fhandle = [kern.comp{i}.type 'KernGradX'];
-    transpose = 0;
     arg{1} = kern.comp{i};
-    factors = kernFactors(kern.comp{i}, 'gradfact');
-    outArg = 1;
 else
     if j<i
         fhandle = [kern.block{i}.cross{j} 'KernGradX'];
@@ -58,14 +58,10 @@ else
     end
     if transpose
         arg{1} = kern.comp{j};
-        factors{1} = kernFactors(kern.comp{j}, 'gradfact');
         arg{2} = kern.comp{i};
-        factors{2} = kernFactors(kern.comp{i}, 'gradfact');
     else
         arg{1} = kern.comp{i};
-        factors{1} = kernFactors(kern.comp{i}, 'gradfact');
         arg{2} = kern.comp{j};
-        factors{2} = kernFactors(kern.comp{j}, 'gradfact');
     end
 end
 fhandle = str2func(fhandle);
@@ -73,16 +69,60 @@ arg{end+1} = X;
 if ~isempty(X2);
     arg{end+1} = X2;
 end
-switch outArg
-    case 1
-        gX = fhandle(arg{:});
-    case 2
-        gX = fhandle(arg{:});
-        if transpose
-            g = g2;
-            g2 = g1;
-            g1 = g;
-        end
-    otherwise
-        error('Invalid number of out arguments.')
-end
+
+gX = fhandle(arg{:}, covGrad);
+
+%/~ the old version of the code
+% if nargin < 6
+%   j = i;
+%   i = covGrad;
+%   covGrad = X2;
+%   X2 = [];
+% end
+% 
+% outArg = 2;
+% if i == j
+%     fhandle = [kern.comp{i}.type 'KernGradX'];
+%     transpose = 0;
+%     arg{1} = kern.comp{i};
+%     factors = kernFactors(kern.comp{i}, 'gradfact');
+%     outArg = 1;
+% else
+%     if j<i
+%         fhandle = [kern.block{i}.cross{j} 'KernGradX'];
+%         transpose = kern.block{i}.transpose(j);
+%     else
+%         fhandle = [kern.block{j}.cross{i} 'KernGradX'];
+%         transpose = ~kern.block{j}.transpose(i);
+%     end
+%     if transpose
+%         arg{1} = kern.comp{j};
+%         factors{1} = kernFactors(kern.comp{j}, 'gradfact');
+%         arg{2} = kern.comp{i};
+%         factors{2} = kernFactors(kern.comp{i}, 'gradfact');
+%     else
+%         arg{1} = kern.comp{i};
+%         factors{1} = kernFactors(kern.comp{i}, 'gradfact');
+%         arg{2} = kern.comp{j};
+%         factors{2} = kernFactors(kern.comp{j}, 'gradfact');
+%     end
+% end
+% fhandle = str2func(fhandle);
+% arg{end+1} = X;
+% if ~isempty(X2);
+%     arg{end+1} = X2;
+% end
+% switch outArg
+%     case 1
+%         gX = fhandle(arg{:}, covGrad);
+%     case 2
+%         gX = fhandle(arg{:}, covGrad);
+%         if transpose
+%             g = g2;
+%             g2 = g1;
+%             g1 = g;
+%         end
+%     otherwise
+%         error('Invalid number of out arguments.')
+% end
+%~\
