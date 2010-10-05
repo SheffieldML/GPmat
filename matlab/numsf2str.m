badce711d7817b00d36da35cc4d1eea6ed92413a
lawrennd <lawrennd@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2010-10-05 22:02:43 +0000

    Moved source code for toolbox paths into new project reproducible.

--- matlab/numsf2str.m
+++ matlab/numsf2str.m
@@ -15,27 +15,31 @@ function str = numsf2str(num, sf);
 
 % NDLUTIL
 
-val = chop(num, sf);
-str = num2str(val, sf);
-tail = [];
-ePos = find(str == 'e');
-if ~isempty(ePos)
-  tail = str(ePos:end);
-  str = str(1:ePos-1);
-end
-ind = 1;
-while str(ind) == '0' | str(ind) == '.'
-  ind = ind+1;
-end
-count = 0;
-while(ind <= length(str))
-  if str(ind) ~= '.'
+  str = num2str(num, sf);
+  tail = [];
+  ePos = find(str == 'e');
+  if ~isempty(ePos)
+    tail = str(ePos+1:end);
+    str = str(1:ePos-1);
+  end
+  ind = 1;
+  while  ind<=length(str) && (str(ind) == '0' || str(ind) == '.')
+    ind = ind+1;
+  end
+  count = 0;
+  while(ind <= length(str))
+    if str(ind) ~= '.'
+      count = count + 1;
+    end
+    ind = ind +1;
+  end
+  while count < sf
+    str = [str '0'];
     count = count + 1;
   end
-  ind = ind +1;
+  if length(tail)>0
+    tail = num2str(str2num(tail));
+    tail = ['\times 10^' tail];
+  end
+  str = [str tail];
 end
-while count < sf
-  str = [str '0'];
-  count = count + 1;
-end
-str = [str tail];
