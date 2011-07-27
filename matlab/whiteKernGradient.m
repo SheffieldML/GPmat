530cbb98078a9e55c6938caabfc0c1dfe7009a9e
Jaakko Peltonen <jaakko.peltonen@aalto.fi>
2011-07-27 15:24:45 +0000

    Extension to white kernel, option to use sigmoidab transform, option to interpolate fixed variances for 1d data

--- matlab/whiteKernGradient.m
+++ matlab/whiteKernGradient.m
@@ -1,51 +1,88 @@
 function g = whiteKernGradient(kern, x, varargin)
 
-% WHITEKERNGRADIENT Gradient of WHITE kernel's parameters.
-% FORMAT
+% WHITEKERNGRADIENT Gradient of white-noise (WHITE) kernel's
+% parameters.
+%
+% FORMAT:
+%
 % DESC computes the gradient of functions with respect to the
-% white noise
-% kernel's parameters. As well as the kernel structure and the
-% input positions, the user provides a matrix PARTIAL which gives
-% the partial derivatives of the function with respect to the
+% white noise kernel's parameters. As well as the kernel structure
+% and the input positions, the user provides a matrix PARTIAL which
+% gives the partial derivatives of the function with respect to the
 % relevant elements of the kernel matrix. 
+%
 % ARG kern : the kernel structure for which the gradients are being
 % computed.
+%
 % ARG x : the input locations for which the gradients are being
 % computed. 
-% ARG partial : matrix of partial derivatives of the function of
-% interest with respect to the kernel matrix. The argument takes
-% the form of a square matrix of dimension  numData, where numData is
-% the number of rows in X.
+%
+% ARG partial (only element of varargin): matrix of partial
+% derivatives of the function of interest with respect to the
+% kernel matrix. The argument takes the form of a square matrix of
+% dimension  numData, where numData is the number of rows in X.
+%
 % RETURN g : gradients of the function of interest with respect to
 % the kernel parameters. The ordering of the vector should match
 % that provided by the function kernExtractParam.
 %
-% FORMAT
+% ALTERNATIVE FORMAT:
+%
 % DESC computes the derivatives as above, but input locations are
 % now provided in two matrices associated with rows and columns of
 % the kernel matrix. 
+%
 % ARG kern : the kernel structure for which the gradients are being
 % computed.
+%
 % ARG x1 : the input locations associated with the rows of the
 % kernel matrix.
-% ARG x2 : the input locations associated with the columns of the
-% kernel matrix.
-% ARG partial : matrix of partial derivatives of the function of
-% interest with respect to the kernel matrix. The matrix should
-% have the same number of rows as X1 and the same number of columns
-% as X2 has rows.
+%
+% ARG x2 (first element of varargin): the input locations
+% associated with the columns of the kernel matrix.
+%
+% ARG partial (second element of varargin): matrix of partial
+% derivatives of the function of interest with respect to the
+% kernel matrix. The matrix should have the same number of rows as
+% X1 and the same number of columns as X2 has rows.
+%
 % RETURN g : gradients of the function of interest with respect to
 % the kernel parameters.
 %
 % SEEALSO whiteKernParamInit, kernGradient, whiteKernDiagGradient, kernGradX
 %
 % COPYRIGHT : Neil D. Lawrence, 2004, 2005, 2006
+%
+% COPYRIGHT : Jaakko Peltonen, 2011
 
 % KERN
 
 
 if nargin < 4
-  g(1, 1) = trace(varargin{end});
+  % If less than 4 parameters are provided, then only a single set
+  % of input locations is used, and the third parameter is the
+  % partial-derivative matrix
+    
+  if (isfield(kern,'use_fixedvariance')) && (kern.use_fixedvariance==1),  
+    % If the special kernel option is used where fixed variances
+    % are interpolated between one-dimensional input locations,
+    % then the kernel variance parameter has no effect on the
+    % kernel and the gradient must be set to zero.
+    g(1, 1) = 0;
+  else
+    % If the kernel is computed as usual as an isotropic
+    % white-noise kernel, then the line below is the correct
+    % gradient with respect to the variance parameter of the kernel.
+    g(1, 1) = trace(varargin{end});
+  end;
 else
+  % If there are at least 4 parameters, then the kernel is being
+  % computed between two different sets of observations: the first
+  % element of varargin is the second set of observation-locations,
+  % and the second element is the partial-derivative matrix. In this
+  % case, because observations are assumed to have independent
+  % noise variables, the kernel is simply a constant (zero) between
+  % any two observations, so the gradient with respect to the
+  % kernel's variance parameter is also zero.
   g = 0;
 end
