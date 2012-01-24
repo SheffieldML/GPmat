d68642ca88fcadac875d66d23542ca50d2c62bab
Antti Honkela <antti.honkela@hiit.fi>
2012-01-24 14:35:55 +0000

    Allow computing pure likelihood for gpnddisim (no prior contribution). Add a message id to allow masking gpsim jitter messages.

--- matlab/gpnddisimLogLikelihood.m
+++ matlab/gpnddisimLogLikelihood.m
@@ -1,10 +1,11 @@
-function ll = gpnddisimLogLikelihood(model)
+function ll = gpnddisimLogLikelihood(model, noprior)
 
 % GPNDDISIMLOGLIKELIHOOD Compute the log likelihood of a GPNDDISIM model.
 % FORMAT
 % DESC computes the log likelihood of the given Gaussian process
 % for use in a single input motif protein network.
 % ARG model : the model for which the log likelihood is computed.
+% ARG noprior : if true, ignore prior (default: false)
 % RETURN ll : the log likelihood of the data set.
 % 
 % SEEALSO : gpsimCreate, gpsimLogLikeGradient, gpsimObjective
@@ -12,13 +13,22 @@ function ll = gpnddisimLogLikelihood(model)
 % COPYRIGHT : Neil D. Lawrence, 2006
 %
 % COPYRIGHT : Jaakko Peltonen, 2011
+%
+% COPYRIGHT : Antti Honkela, 2012
 
 % GPSIM
 
+if nargin < 2,
+  noprior = 0;
+end
 
 dim = size(model.y, 1);
 ll = -dim*log(2*pi) - model.logDetK - model.m'*model.invK*model.m;
 ll = ll*0.5;
+
+if noprior,
+  return;
+end
 
 % In case we need priors in.
 if isfield(model, 'bprior'),
@@ -33,4 +43,3 @@ if isfield(model, 'disimStartMeanPrior'),
   ll = ll + priorLogProb(model.disimStartMeanPrior, ...
 			 model.disimStartMean);
 end
-
