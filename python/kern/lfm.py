kern toolbox: added the fixed effect kernel class (to deal with puma variance) and various bugfixes

python/kern/lfm.py
@@ -1,4 +1,4 @@
-from kern import kern, crosskern
+from kern import kern, cross_kern
 from lfmUpsilon import *
 
 class lfm(kern):
@@ -49,16 +49,16 @@ class lfm(kern):
 	def gradients(self,t1,t2):
 		raise NotImplementedError #TODO
 
-class lfmXlfm(crosskern):
+class lfmXlfm(cross_kern):
 	def __init__(self,lfm1,lfm2):
 		assert isinstance(lfm1, lfm)
 		assert isinstance(lfm2, lfm)#be sure that the kernels we're crossing are both lfm
-		crosskern.__init__(self,lfm1,lfm2)
+		cross_kern.__init__(self,lfm1,lfm2)
 		self.mass1, self.spring1, self.damper1, self.sensitivity1, self.rbfgamma1 = lfm1.get_param()
 		self.mass2, self.spring2, self.damper2, self.sensitivity2, self.rbfgamma2  = lfm2.get_param()
 
 	def function(self,t1,t2):
-		prefactor = self.sensitivity1(self.sensitivity2*np.sqrt(np.pi)*self.lengthscale/8./self.omega1/self.omega2
+		prefactor = self.sensitivity1*self.sensitivity2*np.sqrt(np.pi)*self.lengthscale/8./self.omega1/self.omega2
 		g1,g1_,g2,g2_ = self.kern1.gamma, self.kern1.gamma_, self.kern2.gamma, self.kern2.gamma_
 		k = h(gd_,g1,t1,t2) + h(g1,g2_,t2,t1) + h(g2,g1_,t1,t2) + h(g1_,g2,t2,t1)\
 		  - h(g2_,g1_,t1,t2) - h(g1_,g2_,t2,t1) - h(g2,g1,t1,t2) - h(g1,g2,t2,t1)
@@ -77,9 +77,9 @@ class lfmXlfm(crosskern):
 	def get_param_names(self):
 		return ['mass1', 'spring1', 'damper1', 'sensitivity1', 'rbfgamma1', 'mass2', 'spring2', 'damper2', 'sensitivity2', 'rbfgamma2']
 
-class lfmXrbf(crosskern):
+class lfmXrbf(cross_kern):
 	def __init__(self,lfmkern,rbfkern):
-		crosskern.__init__(self,lfmkern,rbfkern)
+		cross_kern.__init__(self,lfmkern,rbfkern)
 
 	def function(self,t1,t2):
 		raise NotImplementedError #TODO