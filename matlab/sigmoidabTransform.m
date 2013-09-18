6a57aba59fa878bbc7ed1600cadda6dfa7cc895b
Antti Honkela <antti.honkela@hiit.fi>
2013-09-18 13:43:05 +0000

    Optimisations.

--- matlab/sigmoidabTransform.m
+++ matlab/sigmoidabTransform.m
@@ -1,4 +1,4 @@
-function y = sigmoidabTransform(x, transform, varargin)
+function y = sigmoidabTransform(x, transform, transformsettings)
 
 % SIGMOIDABTRANSFORM Constrains a parameter to be between A and B
 % by a scaled logistic sigmoid function.
@@ -36,49 +36,16 @@ function y = sigmoidabTransform(x, transform, varargin)
 % OPTIMI
 
 
-% Get the transformation settings from the varargin structure
-if ~isempty(varargin),
-  transformsettings=varargin{1};
-else
-  transformsettings=[];
-end;
+% compute logistic sigmoid between A and B
+  
+A=transformsettings(1);
+B=transformsettings(2);
 
+limVal = 36;
 
-identitymode=0;
-if identitymode==1,
-  % DEBUG ONLY, turns transformation to identity!
-  fprintf(1,'Using identity mode for transform %s of parameter value %f\n',transform, x)
-  switch transform
-   case 'atox'
-    y=x;
-   case 'xtoa'
-    y=x;
-   case 'gradfact'
-    y=0*x+1;
-  end
-else
-  % normal mode, logistic sigmoid between A and B
-  
-  if isempty(transformsettings),
-    warning(sprintf('sigmoidabTransform(%f,%s) called without transformation settings\n',x,transform));
-    A=0; B=1;
-  else
-    A=transformsettings(1);
-    B=transformsettings(2);
-  end;
-  
-  limVal = 36;
-  minval_sigmoid=A+(B-A)*eps;
-  maxval_sigmoid=A+(B-A)*(1-eps);
-  
-  y = zeros(size(x));
-  switch transform
-   case 'atox'
-    %x
-    %A
-    %B
-    %fprintf(1,'transforming x %f, A %f, B %f\n',x,A,B);
-    
+y = zeros(size(x));
+switch transform
+  case 'atox'
     I1 = x < -limVal;
     y(I1) = A+(B-A)*eps;
 
@@ -88,9 +55,10 @@ else
     I3 = ~I1 & ~I2;
     y(I3) = A+(B-A)*sigmoid(x(I3));
     
-   case 'xtoa'
-    %  [x A B]
-    %fprintf(1,'inverse-transforming x %f, A %f, B %f\n',x,A,B);
+  case 'xtoa'
+    minval_sigmoid=A+(B-A)*eps;
+    maxval_sigmoid=A+(B-A)*(1-eps);
+
     I1 = x<=minval_sigmoid;
     y(I1) = -limVal;
     
@@ -100,13 +68,6 @@ else
     I3 = ~I1 & ~I2;
     y(I3) = invSigmoid((x(I3)-A)/(B-A));
     
-    
-   case 'gradfact'
-    %fprintf(1,'gradfact-transforming x %f, A %f, B %f\n',x,A,B);
-    %y = (B-A)*x.*(1-x);
+  case 'gradfact'
     y = (x-A).*(B-x)/(B-A);
-    %y=0*x+1; % DEBUG ONLY, turns factors off!
-  end
 end;
-
-  
