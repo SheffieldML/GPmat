68681f7c0f759b77af3fc5189e45d74411584093
lawrennd <lawrennd@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2013-11-24 20:13:32 +0000

    General update commit

--- matlab/acclaimReadSkel.m
+++ matlab/acclaimReadSkel.m
@@ -12,10 +12,6 @@ function skel = acclaimReadSkel(fileName)
 
 % MOCAP
 
-% a regular expression for floats
-numPat = '(-?[0-9]*\.?[0-9]*)';
-% a regular expression for positive ints
-intPat = '([0-9]+)';
 
 boneCount = 0;
 fid = fopen(fileName, 'r');
