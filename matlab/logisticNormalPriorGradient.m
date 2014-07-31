20d623e4724224876c9458cd1710abae959bdcb1
Antti Honkela <antti.honkela@hiit.fi>
2014-07-31 15:17:29 +0300

    Clean up normalisation code, does not affect any computed values.

--- matlab/logisticNormalPriorGradient.m
+++ matlab/logisticNormalPriorGradient.m
@@ -7,10 +7,10 @@ function g = logisticNormalPriorGradient(prior, x)
 % Compute gradient of prior
 x0 = (x - prior.a) / (prior.b - prior.a);
 y = invSigmoid(x0);
-g_trans = (x - prior.a) .* (prior.b - x) / (prior.b - prior.a)^3;
+g_trans = (x - prior.a) .* (prior.b - x) / (prior.b - prior.a);
 
 g = -(y - prior.mu) / prior.sd^2 ./ (x0 .* (1-x0)) / (prior.b - prior.a) + ...
-    -1 ./ g_trans .* (prior.a + prior.b - 2*x) / (prior.b - prior.a)^3;
+    -1 ./ g_trans .* (prior.a + prior.b - 2*x) / (prior.b - prior.a);
 
 % invSigmoid(x) = log(x / (1-x))
 % invSigmoid'(x) = 1 / (x .* (1-x))
