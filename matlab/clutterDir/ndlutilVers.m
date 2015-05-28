16124c6ab004639669de260ed0c73c803995161c
Neil Lawrence <lawrennd@gmail.com>
2015-05-28 11:10:57 +0100

    Clean up for GPmat

--- matlab/clutterDir/ndlutilVers.m
+++
@@ -1,8 +0,0 @@
-function [vers, depend] = ndlutilVers
-
-% NDLUTILPATH Brings dependent toolboxes into the path.
-
-vers = 0.131;
-if nargout > 2
-  depend = [];
-end
--- python/ndlutil/__init__.py
+++
@@ -1,5 +0,0 @@
-from parameterised import parameterised
-from model import model
-from utilities import jitchol, pdinv
-from mdot import mdot
-__all__ = ['samplers', 'model','jitchol','pdinv','parameterised','Tango','mdot']
