9d2780cf6fb2a2b1714cf727d230f2f4e94772b6
Neil Lawrence <lawrennd@gmail.com>
2015-05-28 21:36:55 +0100

    Move files into mltools.

--- gpmmltools/dnetTest.m
+++
@@ -1,9 +0,0 @@
-% DNETTEST Test some settings for the density network.
-
-% MLTOOLS
-
-model = dnetCreate(2, 3, randn(2, 3), dnetOptions);
-
-model.basisStored = false;
-
-modelTest(model)  
--- gpmmltools/vectorModify.m
+++
@@ -1,7 +0,0 @@
-function handle = vectorModify(handle, values)
-
-% VECTORMODIFY Helper code for visualisation of vectorial data.
-
-% MLTOOLS
-
-set(handle, 'YData', values);
