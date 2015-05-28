27c44d3da13b2803777282e2ec54e131219d8cf6
Neil Lawrence <lawrennd@gmail.com>
2015-05-28 21:01:24 +0100

    Move files into datasets.

--- gpmdatasets/xlsLoadData.m
+++
@@ -1,8 +0,0 @@
-function [data, names] = xlsLoadData(fileName, sheetName)
-
-% XLSLOADDATA Wrapper function for xlsread to get files from the datasets directory.
-
-% DATASETS
-
-baseDir = datasetsDirectory;
-[data, names] = xlsread([baseDir fileName], sheetName);
