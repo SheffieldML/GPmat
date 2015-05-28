b6cebd56e8837691ca1bfaebdb5aae86b60a26ee
Neil Lawrence <lawrennd@gmail.com>
2015-05-28 21:51:56 +0100

    Move files into noise.

--- gpmnoise/ncnmNoiseSites.m
+++
@@ -1,9 +0,0 @@
-function [m, beta] = ncnmNoiseSites(noise, g, nu, mu, varSigma, y)
-
-% NCNMNOISESITES Site updates for null category model.
-
-% NOISE
-
-% The standard code.
-beta = nu./(1-nu.*varSigma);
-m = mu + g./nu;
