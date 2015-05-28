c99fc8665e2c1169ba3b71fd15c83ffddfebf478
Neil Lawrence <lawrennd@gmail.com>
2015-05-28 21:43:59 +0100

    Move files into ndlutil.

--- gpmndlutil/escapeText.m
+++
@@ -1,8 +0,0 @@
-function txt = escapeText(txt)
-  
-% ESCAPETEXT Add back slashes to escape existing backslashes in a text.
-
-% NDLUTIL
-  
-txt = strrep(txt, '\', '\\');
-txt = strrep(txt, '%', '%%');
--- gpmndlutil/sigmoid.m
+++
@@ -1,7 +0,0 @@
-function y = sigmoid(x)
-
-% SIGMOID The sigmoid function
-
-% NDLUTIL
-
-y = ones(size(x))./(1+exp(-x));
