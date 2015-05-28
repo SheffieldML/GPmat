ab43248ea118ef0b8131efee5bec019f30ed825a
Neil Lawrence <lawrennd@gmail.com>
2015-05-28 21:51:56 +0100

    Move files into noise.

--- gpmnoise/scaleNoiseExpandParam.m
+++
@@ -1,8 +0,0 @@
-function noise = scaleNoiseExpandParam(noise, params)
-
-% SCALENOISEEXPANDPARAM Expand Scale noise structure from param vector.
-
-% NOISE
-
-noise.bias = params(1:noise.numProcess);
-noise.scale = params(noise.numProcess+1:end);
