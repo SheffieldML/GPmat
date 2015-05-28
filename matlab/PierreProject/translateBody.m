f4b66317c9ba2c771496024d75a5dfd0603f2a12
Neil Lawrence <lawrennd@gmail.com>
2015-05-28 08:14:56 +0100

    Remove student project code from pierre

--- matlab/PierreProject/translateBody.m
+++
@@ -1,7 +0,0 @@
-function matrix2beTranslated = translateBody(matrix2beTranslated,z)  
-[nline ncol] = size(matrix2beTranslated);
-
-    for j = 1:nline
-        matrix2beTranslated(j,3)= matrix2beTranslated(j,3)+z ;
-    end
-
