ff44bc28b45ef3b6ed79000af208b68062983c64
Neil Lawrence <lawrennd@gmail.com>
2015-05-28 21:34:33 +0100

    Move files into kern.

--- gpmkern/lfmComputeUpsilonDiagVector.m
+++
@@ -1,9 +0,0 @@
-function upsilon = lfmComputeUpsilonDiagVector(gamma, sigma2, t)
-
-% LFMCOMPUTEUPSILONDIAGVECTOR 
-%
-% COPYRIGHT : Mauricio A. Alvarez, 2010
-
-% KERN
-
-upsilon = exp(-gamma*t).*lfmComputeUpsilonVector(-gamma ,sigma2, t);
--- gpmkern/tensorKernSetIndex.m
+++
@@ -1,7 +0,0 @@
-function kern = tensorKernSetIndex(kern, component, indices)
-
-% TENSORKERNSETINDEX Set the indices in the tensor kernel.
-
-% KERN
-
-kern = cmpndKernSetIndex(kern, component, indices);
